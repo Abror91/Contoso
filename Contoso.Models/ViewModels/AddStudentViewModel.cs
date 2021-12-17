1e1a99d7516eb1918c0c9e624bd0907fc323e63d
Abror Kurbanov <abror_kurbanov@epam.com>
2021-12-17 23:08:11 +0500

    Addition functionalities added

---
+++ Contoso.Models/ViewModels/AddStudentViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Contoso.Models.Entities;
+using System;
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contoso.Models.ViewModels
+{
+    public class AddStudentViewModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public int? DepartmentId { get; set; }
+
+        public Student ToEntity()
+        {
+            var student = new Student()
+            {
+                FirstName = FirstName,
+                LastName = LastName,
+                Email = Email,
+                DepartmentId = (int)DepartmentId
+            };
+            return student;
+        }
+    }
+}
